c7d0a755b8ee8484fb935509de56878182015763
BykadorovR <bykadorov.r@gmail.com>
2025-12-02 21:15:40 +0100

    Revert "fix: removed old descriptor set/pool"
    This reverts commit 1d3022ff6f0a056d36061c085852475e07b92857.

--- src/DescriptorSet.cpp
+++ src/DescriptorSet.cpp
@@ -36,6 +36,62 @@ DescriptorSetLayout::~DescriptorSetLayout() {
   vkDestroyDescriptorSetLayout(_device->getLogicalDevice(), _descriptorSetLayout, nullptr);
 }
 
+DescriptorSet::DescriptorSet(const DescriptorSetLayout& layout, DescriptorPool& descriptorPool, const Device& device)
+    : _descriptorPool(&descriptorPool),
+      _device(&device) {
+  _layoutInfo = layout.getLayoutInfo();
+
+  _descriptorPool->notify(_layoutInfo, 1);
+
+  auto descriptorLayout = layout.getDescriptorSetLayout();
+  VkDescriptorSetAllocateInfo allocInfo{.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
+                                        .descriptorPool = _descriptorPool->getDescriptorPool(),
+                                        .descriptorSetCount = 1,
+                                        .pSetLayouts = &descriptorLayout};
+  auto sts = vkAllocateDescriptorSets(device.getLogicalDevice(), &allocInfo, &_descriptorSet);
+  if (sts != VK_SUCCESS) {
+    std::ostringstream descriptors;
+    descriptors << "failed to allocate descriptor sets: allocated sets: " << descriptorPool.getDescriptorSetsNumber()
+                << ", descriptors: ";
+    for (auto&& [key, value] : descriptorPool.getDescriptorsNumber()) {
+      descriptors << key << ":" << value << " ";
+    }
+    throw std::runtime_error(descriptors.str());
+  }
+}
+
+void DescriptorSet::updateCustom(const std::map<int, std::vector<VkDescriptorBufferInfo>>& buffers,
+                                 const std::map<int, std::vector<VkDescriptorImageInfo>>& images) noexcept {
+  std::vector<VkWriteDescriptorSet> descriptorWrites;
+  descriptorWrites.reserve(buffers.size() + images.size());
+  for (auto&& [key, value] : buffers) {
+    VkWriteDescriptorSet descriptorSet = {.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+                                          .dstSet = _descriptorSet,
+                                          .dstBinding = _layoutInfo[key].binding,
+                                          .dstArrayElement = 0,
+                                          .descriptorCount = _layoutInfo[key].descriptorCount,
+                                          .descriptorType = _layoutInfo[key].descriptorType,
+                                          .pBufferInfo = buffers.at(key).data()};
+    descriptorWrites.push_back(descriptorSet);
+  }
+  for (auto&& [key, value] : images) {
+    VkWriteDescriptorSet descriptorSet = {.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+                                          .dstSet = _descriptorSet,
+                                          .dstBinding = _layoutInfo[key].binding,
+                                          .dstArrayElement = 0,
+                                          .descriptorCount = _layoutInfo[key].descriptorCount,
+                                          .descriptorType = _layoutInfo[key].descriptorType,
+                                          .pImageInfo = images.at(key).data()};
+    descriptorWrites.push_back(descriptorSet);
+  }
+
+  vkUpdateDescriptorSets(_device->getLogicalDevice(), descriptorWrites.size(), descriptorWrites.data(), 0, nullptr);
+}
+
+VkDescriptorSet DescriptorSet::getDescriptorSet() const noexcept { return _descriptorSet; }
+
+DescriptorSet::~DescriptorSet() { _descriptorPool->notify(_layoutInfo, -1); }
+
 DescriptorBuffer::DescriptorBuffer(const DescriptorSetLayout& layout, 
                                    const MemoryAllocator& memoryAllocator,
                                    const Device& device) {
